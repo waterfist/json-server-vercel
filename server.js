7dfe1636fbbde4d6eeca3fdafe95ff8f34b202b6
hotsix <codinggo@naver.com>
2022-12-27 22:01:26 +0900

    qwe

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 // See https://github.com/typicode/json-server#module
-const fs = require("fs");
-const os = require("os");
-
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
-  if (err) console.log(err);
-  else console.log("copy file succeed to" + os.tmpdir());
-});
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
